d6579665141becacc586307916b041c88440c1a8
Christine Nguyen <ctine.ngyn@gmail.com>
2021-04-10 22:46:25 -0700

    Promise chains

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
+const { resolve } = require("path");
 
 function runInquirer() {
     const promptArray = [{
@@ -52,4 +53,53 @@ function runInquirerEngineer() {
 
     return inquirer
         .prompt(promptArray);
+}
+
+function runInquirerIntern() {
+    const promptArray = [{
+        type: "input",
+        message: "What school do you attend?",
+        name: "school"
+    }];
+
+    return inquirer
+        .prompt(promptArray);
+}
+
+
+async function run() {
+    let employeeArray = [];
+    const maxTimes = 4;
+    for (i = 0; i < maxTimes; i++) {
+        const promise = new Promise((resolve, reject) => {
+            runInquirer()
+                .then(function({ name, id, email, title }) {
+
+                    if (title === "Manager") {
+                        runInquirerManager().then(function(officeNumber)) {
+                            this.employee = new Manager(name, id, email, officeNumber);
+                            console.log(officeNumber);
+                            employeeArray.push(employee);
+                            resolve("done");
+                        });
+
+                    } else if (title === "Engineer") {
+                        runInquirerEngineer().then(function({ github }) {
+                            this.employee = new Engineer(name, id, email, github);
+                            console.log(github);
+                            employeeArray.push(employee);
+                            resolve("done");
+                        });
+
+                    } else if (title === "Intern") {
+                        runInquirerIntern().then(function({ school }) {
+                            this.employee = new Intern(name, id, email, school);
+                            console.log(school)
+                            employeeArray.push(employee);
+                            resolve("done");
+                        });
+                    }
+                }
+        }
+    }
 }
