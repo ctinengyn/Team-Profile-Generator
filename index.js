be18bdb055094909d27af8c9542e9f2a46407b58
Christine Nguyen <ctine.ngyn@gmail.com>
2021-04-10 22:20:18 -0700

    Update

--- index.js
+++ index.js
@@ -1,8 +1,55 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 
+function runInquirer() {
+    const promptArray = [{
+        type: "input",
+        message: "What is your name?",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "What is your ID?",
+        name: "id"
+    },
+    {
+        type: "input",
+        messgae: "What is your emalil?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "Would you like to add another employee?",
+        choices: ["Engineer", "Intern", "Manager"],
+        name: "title"
+    }];
+
+    return inquirer
+        .prompt(promptArray);
+}
+
+function runInquirerManager() {
+    const promptArray = [{
+        type: "input",
+        message: "What is your office number?",
+        name: "office number"
+    }];
+
+    return inquirer
+        .prompt(promptArray);
+}
+
+function runInquirerEngineer() {
+    const promptArray = [{
+        type: "input",
+        message: "What is your GitHub?",
+        name: "github"
+    }];
+
+    return inquirer
+        .prompt(promptArray);
+}
