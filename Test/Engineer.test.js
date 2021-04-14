46d43cf0c1cc595f3cc22ca3ea4b6b4c78e73b3c
Christine Nguyen <ctine.ngyn@gmail.com>
2021-04-14 00:04:49 -0700

    Final test suites for Engineer

--- Test/Engineer.test.js
+++ Test/Engineer.test.js
@@ -5,3 +5,15 @@ test("Can set GitHub", () => {
     const e = new Engineer("Nguyen", 123, "testy@test.com", testValue);
     expect(e.github).toBe(testValue);
 });
+
+test("getRole()", () => {
+    const testValue = "Engineer";
+    const e = new Engineer("Nguyen", 123, "testy@test.com", "GitHubUser");
+    expect(e.getRole()).toBe(testValue);
+});
+
+test("Can get GitHub via getGithub()", () => {
+    const testValue = "GitHubUser";
+    const e = new Engineer("Nguyen", 123, "testy@test.com", testValue);
+    expect(e.getGithub()).toBe(testValue);
+});
