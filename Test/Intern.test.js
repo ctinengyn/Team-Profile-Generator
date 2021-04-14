38069142f5669d76d31f42cdc351ddd01a87c927
Christine Nguyen <ctine.ngyn@gmail.com>
2021-04-14 00:08:34 -0700

    Fianl test suites for Intern

--- Test/Intern.test.js
+++ Test/Intern.test.js
@@ -6,3 +6,14 @@ test("Can set school", () => {
     expect(e.school).toBe(testValue);
 });
 
+test("getRole()", () => {
+    const testValue = "Intern";
+    const e = new Intern("Nguyen", 123, "testy@test.com", "UW");
+    expect(e.getRole()).toBe(testValue);
+});
+
+test("Can get school via getSchool()", () => {
+    const testValue = "UW";
+    const e = new Intern("Nguyen", 123, "testy@test.com", testValue);
+    expect(e.getSchool()).toBe(testValue);
+});
