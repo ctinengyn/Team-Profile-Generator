43c684c9b7acf701416e6c4c8404aaceb1389bda
Christine Nguyen <ctine.ngyn@gmail.com>
2021-04-13 23:55:31 -0700

    Final test suites for Employee

--- Test/Employee.test.js
+++ Test/Employee.test.js
@@ -10,3 +10,39 @@ test("Can set name", () => {
     const e = new Employee(name);
     expect(e.name).toBe(name);
 });
+
+test("Can set id", () => {
+    const testValue = "123";
+    const e = new Employee("Nguyen", testValue);
+    expect(e.id).toBe(testValue);
+});
+
+test("Can set email", () => {
+    const testValue = "testy@test.com";
+    const e = new Employee("Nguyen", 123, testValue);
+    expect(e.email).toBe(testValue);
+});
+
+test("Can set name via getName()", () => {
+    const testValue = "Christine";
+    const e = new Employee(testValue);
+    expect(e.getName()).toBe(testValue);
+});
+
+test("Can set name via getId()", () => {
+    const testValue = 123;
+    const e = new Employee("Nguyen", testValue);
+    expect(e.getId()).toBe(testValue);
+});
+
+test("Can set name via getEmail()", () => {
+    const testValue = "testy@test.com";
+    const e = new Employee("Nguyen", 123, testValue);
+    expect(e.getEmail()).toBe(testValue);
+});
+
+test("getRole()", () => {
+    const testValue = "Employee";
+    const e = new Employee("Christine", 123, "testy@test.com");
+    expect(e.getRole()).toBe(testValue);
+});
