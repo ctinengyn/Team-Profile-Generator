02f5db80a988744d43f44441aa9509c14dff84d3
Christine Nguyen <ctine.ngyn@gmail.com>
2021-04-16 01:10:28 -0700

    Adding arrays

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const fs = require("fs");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
-const { resolve } = require("path");
+const Employee = require("./lib/Employee");
+
+let managerArray = [];
+let engineerArray = [];
+let internArray = [];
 
 function runInquirer() {
     const promptArray = [{
